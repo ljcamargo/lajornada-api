bc219cec233cfab56bfaa3185e1971f4750c2470
Luis J Camargo <lsjcp@yahoo.com>
2013-12-11 20:09:31 -0600

    noteid bug solved

--- jornada/json/updater.py
+++ jornada/json/updater.py
@@ -143,7 +143,7 @@ class Updater(object):
 
         
     def pushThisNote(self, text, link, noteid):
-        noteid = self.getMicroID(noteid)
+        #noteid = self.getMicroID(noteid)
         appcode = "EA506-AAE0E"
         #appcode = "27D11-8F224" #debugging appcode
         token = "5y2m8EkDJ1urdRPQfIFYpQguNhxXqBk/nvyx7vKnANrUpsseqvN6VmiNJuUPfosXrcE0BWpORQlK9/c9nvgY"
